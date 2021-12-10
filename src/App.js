fe83ebfdb10707238ee89eb52cdc839a72631a88
DenoelaGuennoc <dguennoc@gmail.com>
2021-12-10 14:55:20 +0100

    Modification titre, favicon, logo, texte de présentation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenue sur l'interface de votre moniteur cardiaque.
         </p>
         <a
           className="App-link"
