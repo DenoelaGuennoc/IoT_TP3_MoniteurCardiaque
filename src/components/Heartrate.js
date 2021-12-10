3ad0194437578fbd5172676d586daac5072523b5
DenoelaGuennoc <dguennoc@gmail.com>
2021-12-10 15:40:35 +0100

    Affichage 1 Bpm

--- src/components/Heartrate.js
+++ src/components/Heartrate.js
@@ -1,7 +1,15 @@
 function Heartrate() {
-    return (
-        <p>Nouveau Composant</p>
-    )
+    var rythmeCardiaque = 40 + Math.floor(Math.random() * (200 - 40));
+    var pbBpm = ""
+    var description = " "
+    if (rythmeCardiaque<50 || rythmeCardiaque>150) {
+        pbBpm = "pbBpm";
+    }
+    else {
+        pbBpm ="bpmOk";  
+    }
+    description = rythmeCardiaque + " bpm";
+    return <div class={pbBpm}> {description} </div>;
 }
 
 export default Heartrate;
